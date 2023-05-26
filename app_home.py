ef4d32fdab5d434d80b0a1128324016c285f2744
wnstjq0915 <wnstjq0915@gmail.com>
2023-05-26 14:36:21 +0900

    자동배포 처리 완료

--- app_home.py
+++ app_home.py
@@ -3,6 +3,7 @@ import streamlit as st
 def fun_app_home():
     st.subheader('환영합니다~')
     st.text('좋은 서비스로 제공하겠습니다.')
+    st.text('자동 배포 처리된 앱입니다.')
 
     st.text('이 앱은 고객 데이터와 자동차 구매 데이터에 대한 내용입니다.')
     st.text('데이터 분석도 가능하고, 고객 정보를 넣으면 구매 차량 가격도 예측해 줍니다.')
